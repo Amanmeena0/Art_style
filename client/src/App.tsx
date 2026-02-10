220a22ac1eb38e1b8580c8c640a1fc2acc1ace6e
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-10 22:12:09 +0530

    update

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-300">
+      <p className="font-sans">
+        Hi this aman here
+      </p>
+      </div>
+    </>
+  )
+}
+
+export default App
