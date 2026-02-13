a1a55912993dd6729200097001a6b9d0aca4f43a
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-13 13:02:15 +0530

    meesaage

--- Server/tools/gradientDescent.py
+++ Server/tools/gradientDescent.py
@@ -18,12 +18,12 @@ style_targets = extractor(style_image)['style']
 content_targets = extractor(content_image)['content']
 
 
-image = tf.Variable(content_targets)
+image = tf.Variable(content_image)
 
 def clip_0_1(image):
     return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
 
-opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilor=1e-1)
+opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
 
 style_weight = 1e-2
 content_weight = 1e4
@@ -31,10 +31,10 @@ content_weight = 1e4
 def style_content_loss(outputs):
     style_outputs = outputs['style']
     content_outputs = outputs['content']
-    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) for name in content_outputs.keys()])
+    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) for name in style_outputs.keys()])
     style_loss *= style_weight/ num_style_layers
 
-    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name] - content_targets[name])**2 ) for name in content_outputs.key()])
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name] - content_targets[name])**2 ) for name in content_outputs.keys()])
 
     content_loss *= content_weight/ num_content_layers
 
