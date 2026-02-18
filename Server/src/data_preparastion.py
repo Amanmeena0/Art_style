a2bc1397bda3ab7e5cbea07b97ab7892e762c85a
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-18 16:46:28 +0530

    message

--- Server/src/data_preparastion.py
+++ Server/src/data_preparastion.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 
-
-
 content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
 style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
 
