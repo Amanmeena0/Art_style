0f4aea52e5bfed5cd7852c333bc8b7cd887bccd5
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-18 16:38:55 +0530

    message

--- Server/api/connectiions.py
+++
@@ -1,5 +0,0 @@
-from database import MongoDBConnection
-
-db_connection = MongoDBConnection()
-
-__all__ = ['MongoDBConnection', 'db_connection']
--- Server/src/data_preparastion.py
+++ Server/src/data_preparastion.py
@@ -1,82 +1,39 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import os
-
-DEFAULT_CONTENT_PATH = '../images/content/pexels-philippe-alamazani-508356-28479799.jpg'
-DEFAULT_STYLE_PATH = '../images/style/download.jpeg'
 
 
-def load_image(path_to_img: str, max_dim: int = 512) -> tf.Tensor:
-    img = tf.io.read_file(path_to_img)
-    img = tf.image.decode_image(img, channels=3)
-    img = tf.cast(img, tf.float32)
-    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-    long_dim = max(shape)
-    scale = max_dim / long_dim
-    new_shape = tf.cast(shape * scale, tf.int32)
-    img = tf.image.resize(img, new_shape)
-    img = img / 255.0
-    img = img[tf.newaxis, :]
-    return img
 
+content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
+style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
 
-def load_image_from_bytes(image_bytes: bytes, max_dim: int = 512) -> tf.Tensor:
-    img = tf.image.decode_image(image_bytes, channels=3)
-    img = tf.cast(img, tf.float32)
-    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-    long_dim = max(shape)
-    scale = max_dim / long_dim
-    new_shape = tf.cast(shape * scale, tf.int32)
-    img = tf.image.resize(img, new_shape)
-    img = img / 255.0
-    img = img[tf.newaxis, :]
-    return img
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
 
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
 
 def imshow(image, title=None):
-    if len(image.shape) > 3:
-        image = tf.squeeze(image, axis=0)
-    plt.imshow(image)
-    if title:
-        plt.title(title)
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
 
+  plt.imshow(image)
+  if title:
+    plt.title(title)
 
-class ImageProcessor:
-    def __init__(self, max_dim: int = 512):
-        self.max_dim = max_dim
-        self.content_image = None
-        self.style_image = None
-    
-    def load_content_from_path(self, path: str):
-        self.content_image = load_image(path, self.max_dim)
-        return self.content_image
-    
-    def load_style_from_path(self, path: str):
-        self.style_image = load_image(path, self.max_dim)
-        return self.style_image
-    
-    def load_content_from_bytes(self, image_bytes: bytes):
-        self.content_image = load_image_from_bytes(image_bytes, self.max_dim)
-        return self.content_image
-    
-    def load_style_from_bytes(self, image_bytes: bytes):
-        self.style_image = load_image_from_bytes(image_bytes, self.max_dim)
-        return self.style_image
-    
-    def get_images(self):
-        return self.content_image, self.style_image
+content_image = load_img(content_path)
+style_image = load_img(style_path)
 
+# plt.subplot(1, 2, 1)
+# imshow(content_image, 'Content Image')
 
-# Load default images for backward compatibility
-content_image = load_image(DEFAULT_CONTENT_PATH) if os.path.exists(DEFAULT_CONTENT_PATH) else None
-style_image = load_image(DEFAULT_STYLE_PATH) if os.path.exists(DEFAULT_STYLE_PATH) else None
-
-# Uncomment below for visualization
-# plt.subplot(1,2,1)
-# imshow(content_image,'Content Image')
-
-# plt.subplot(1,2,2)
-# imshow(style_image,'Style Image')
-
-# plt.show()
-
+# plt.subplot(1, 2, 2)
+# imshow(style_image, 'Style Image')
