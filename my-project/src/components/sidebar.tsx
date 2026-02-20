3c29e431d447ab10e8b92b00f22105cc7ae53df2
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-20 16:20:33 +0530

    Add backend and frontend folders

---
+++ my-project/src/components/sidebar.tsx
@@ -0,0 +1,15 @@
+export function SideBar(){
+
+    return (
+        <div className="bg-blue-400 h-screen w-32">
+            
+            <div > LOGO</div>
+
+            <div> New chat </div>
+
+            <div> Past chat</div>
+
+            <div className="flex"> Profile </div>
+        </div>
+    )
+}
