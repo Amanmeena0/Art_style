9dfc629ea52f19ac59959375f67a9f3d5daf27b4
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-24 23:07:04 +0530

    update

--- Server/database/server.js
+++ Server/database/server.js
@@ -1,23 +1,12 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
-app.use(express.json());
+const port  = 3000;
 
-app.post('/api/analyze', async (req, res) => {
-    try {
-        const userData = req.body; // e.g., { text: "Hello AI" }
 
-        // Forward the request to your Python server
-        const response = await axios.post('http://localhost:8000/predict', userData);
-        
-        // Send the ML result back to the frontend
-        res.json({
-            status: "success",
-            ml_results: response.data
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Python server is down!" });
-    }
-});
+app.get('/', (req, res) => {
+    res.send('Fuck you bitch');
+})
 
-app.listen(3000, () => console.log('Node server running on port 3000'));
+app.listen(port, () => {
+    console.log(`server listening at port: ${port}`);
+})
