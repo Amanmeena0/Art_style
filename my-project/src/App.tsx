c860092105039d711160b0094f21bd7bff3aef66
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-10 17:18:45 +0530

    updated code

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "@/components/UiView"
 import Grid from "@/components/mainGrid";
-import { ClerkLoading, ClerkLoaded, Show, SignIn, UserButton } from '@clerk/react'
+import { ClerkLoading, ClerkLoaded, Show, SignIn } from '@clerk/react'
 
 
 function App() {
@@ -19,9 +19,6 @@ function App() {
         </Show>
         
         <Show when="signed-in">
-          <header className="flex items-center justify-end gap-4 p-4">
-            <UserButton />
-          </header>
           <Navbar />
           <div className="flex flex-1 overflow-hidden relative w-full">
             <Grid/>
--- my-project/src/components/UiView.tsx
+++ my-project/src/components/UiView.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from '@clerk/react'
+
 export function Navbar(){
     return (
         <div className="flex items-center justify-between bg-linear-to-r from-blue-700 via-blue-600 to-blue-700 text-white px-3 sm:px-4 md:px-6 py-3 sm:py-4 shadow-lg gap-2 sm:gap-4">
@@ -11,8 +13,9 @@ export function Navbar(){
                 <h1 className="text-sm sm:text-base md:text-xl font-bold tracking-wide animate-pulse line-clamp-1">Art Style Image Transformation</h1>
             </div>
             
-            {/* Spacer for alignment */}
-            <div className="w-8 sm:w-9"></div>
+            <div className="flex items-center">
+                <UserButton afterSignOutUrl="/" />
+            </div>
         </div>
         
     );
