1385dface7c05fa423972abb23daeb7c6b692326
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-10 16:35:11 +0530

    message

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,14 +1,25 @@
 import { Navbar } from "@/components/UiView"
 import Grid from "@/components/mainGrid";
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 
 function App() {
   return (
     <div className="h-screen w-screen flex flex-col overflow-hidden">
-     <Navbar />
-     <div className="flex flex-1 overflow-hidden relative w-full">
-      <Grid/>
-     </div>
+      <header className="flex items-center justify-end gap-4 p-4">
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+      <Navbar />
+      <div className="flex flex-1 overflow-hidden relative w-full">
+        <Grid/>
+      </div>
     </div>
   )
 }
--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/react'
+
+
+const ApiKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={ApiKey}>
+      <App />
+    </ClerkProvider>
+  
   </StrictMode>,
 )
